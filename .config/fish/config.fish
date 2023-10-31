4bbb533787007c24313e9753e2badf5a5b9bc33d
archdsp <archdsp01@gmail.com>
2023-11-01 05:36:14 +0900

    Add drawing line on fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,10 @@ set GIT_CURL_VERBOSE 1
 set GIT_TRACE 1
 set LESSCHARSET utf-8
 
+function border --on-event fish_postexec
+    string repeat --count $COLUMNS ─
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
