ed1637e731edbc873c5f7bae747d210aacdd47bc
archdsp <archdsp01@gmail.com>
2024-01-17 13:32:46 +0900

    Move .emacs to init.el under .emacs.d and change url of elpa

---
+++ .emacs.d/init.el
@@ -0,0 +1,42 @@
+(global-display-line-numbers-mode t)
+
+(global-hl-line-mode t)
+(whitespace-mode t)
+
+(global-set-key [f10] 'describe-function)
+(global-set-key [f12] 'describe-key)
+
+; window key
+(global-set-key (kbd "M-9") 'split-window-vertically)
+(global-set-key (kbd "M-0") 'split-window-horizontally)
+(global-set-key (kbd "M-p") 'delete-window)
+(global-set-key (kbd "M-_") 'shrink-window-horizontally)  
+(global-set-key (kbd "M-+") 'enlarge-window-horizontally)
+(global-set-key (kbd "M--") 'shrink-window)
+(global-set-key (kbd "M-=") 'enlarge-window)
+
+(global-set-key (kbd "C-c <left>")  'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+(global-set-key (kbd "C-c <up>")    'windmove-up)
+(global-set-key (kbd "C-c <down>")  'windmove-down)
+
+;; PACKAGE
+(require 'package)
+(setq package-enable-at-startup t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("elpa" ."https://elpa.gnu.org/packages/") t)
+(package-initialize)
+(put 'scroll-left 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(lsp-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
